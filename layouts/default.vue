d425b507b81ddd3736d8e08c36ebcc9aaa918250
himyjan <51815522+himyjan@users.noreply.github.com>
2023-09-21 16:37:47 +0800

    upload code

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from '../components/Header/Header.vue';
+import Footer from '../components/Footer/Footer.vue';
+import { store } from '../store/store';
+</script>
+
+<template>
+  <!-- <Header :cartItems="store.cartItems" /> -->
+  <Header :cartItems="[]" />
+  <slot />
+  <Footer />
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { injectGlobal } from '@magister_zito/vue3-styled-components';
+import Home from '../components/Home/Home.vue';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+
+  #__nuxt {
+    min-height: 100vh;
+    padding: 140px 0 115px;
+    position: relative;
+
+    @media screen and (max-width: 1279px) {
+      padding: 102px 0 208px;
+    }
+  }
+`;
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped></style>
